60b2324939cf182020568b74b61875577b41cd64
Ben W <gadgetwolf1996@outlook.com>
2019-11-12 15:00:45 +0000

    Vehicle movement. Obstacle Controller
    Vehicle movement. Obstacle Controller

--- Assets/Scripts/ObstacleMovement.cs
+++ Assets/Scripts/ObstacleMovement.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class ObstacleMovement : MonoBehaviour
 {
+    Rigidbody rb;
+    public float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = transform.right * speed;
     }
 }
