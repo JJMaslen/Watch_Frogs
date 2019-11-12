4471a530b30714a1b0e86c885230dca373d4c44d
Ben W <gadgetwolf1996@outlook.com>
2019-11-12 16:29:49 +0000

    Update: Obstacle
    - Obstacle now despawns when colliding with exits/entrances
    - Lanes are created
    - Vehicle is restricted to forwards motion

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -6,10 +6,11 @@ public class ObstacleSpawner : MonoBehaviour
 {
     public GameObject obst;
     public Transform [] spawns;
-    private bool nsc; //no spawnables catcher
+    private bool nsc, call; //no spawnables catcher
     // Start is called before the first frame update
     void Start()
     {
+        call = true;
         if (obst == null)
         {
             nsc = true;
@@ -19,10 +20,10 @@ public class ObstacleSpawner : MonoBehaviour
             nsc = false;
         }
 
-        for (int i = 0; i < this.gameObject.transform.childCount; i++)
-        {
-            spawns[i] = this.gameObject.transform.GetChild(i);
-        }
+        //for (int i = 0; i < ; i++)
+        //{
+        //    spawns[i] = this.gameObject.transform.GetChild(i);
+        //}
     }
 
     // Update is called once per frame
@@ -30,7 +31,19 @@ public class ObstacleSpawner : MonoBehaviour
     {
         if (!nsc)
         {
-            Instantiate(obst, spawns[Random.Range(0, spawns.Length)]);
+            if (call)
+            {
+                StartCoroutine(Delay());
+            }
         }
+    }
+
+    IEnumerator Delay()
+    {
+        call = false;
+        yield return new WaitForSeconds(1);
+        Transform temp = spawns[Random.Range(0, spawns.Length)].transform;
+        Instantiate(obst, temp.position, temp.rotation);
+        call = true;
     }
 }
