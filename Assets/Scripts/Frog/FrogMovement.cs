0a36a3091b718161ed2684caec494b02dd3a7cdb
Ash <Huskitch@users.noreply.github.com>
2019-11-19 14:48:54 +0000

    Adjusted frog movement

--- Assets/Scripts/Frog/FrogMovement.cs
+++ Assets/Scripts/Frog/FrogMovement.cs
@@ -20,22 +20,22 @@ public class FrogMovement : MonoBehaviour
     {
 		transform.position = Vector3.Lerp(transform.position, targetPosition, MovementSpeed);
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.A))
 		{
 			targetPosition += new Vector3(-MovementAmount, 0, 0);
 		}
 
-		if (Input.GetKeyDown(KeyCode.S))
+		if (Input.GetKeyDown(KeyCode.D))
 		{
 			targetPosition += new Vector3(MovementAmount, 0, 0);
 		}
 
-		if (Input.GetKeyDown(KeyCode.A))
+		if (Input.GetKeyDown(KeyCode.S))
 		{
 			targetPosition += new Vector3(0, 0, -MovementAmount);
 		}
 
-		if (Input.GetKeyDown(KeyCode.D))
+		if (Input.GetKeyDown(KeyCode.W))
 		{
 			targetPosition += new Vector3(0, 0, MovementAmount);
 		}
