401ebbbeb44deb6967c35fcc44a74b53bd14bf42
JJMaslen <44002981+JJMaslen@users.noreply.github.com>
2019-11-25 17:27:08 +0000

    Arrow is now a prefab and animates

---
+++ Assets/Scripts/ArrowAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowAnimation : MonoBehaviour
+{
+	public GameObject Arrow;
+	Vector3 currentPosition;
+
+	float bob;
+	float rotate;
+	// Start is called before the first frame update
+	void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		bob = (Mathf.Sin(Time.fixedTime) / 2);
+		rotate += 1;
+
+		currentPosition = Arrow.transform.position;
+		Arrow.transform.position = new Vector3(currentPosition.x, bob, currentPosition.z);
+		Arrow.transform.rotation = Quaternion.Euler(-90, rotate, 0);
+    }
+}
