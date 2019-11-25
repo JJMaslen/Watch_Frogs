124042b672dfbe920c5fe340c157316d480f3d1b
JJMaslen <44002981+JJMaslen@users.noreply.github.com>
2019-11-25 16:21:29 +0000

    Log Prefab has Frog collision

---
+++ Assets/Scripts/LogFrogInteraction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogFrogInteraction : MonoBehaviour
+{
+	public GameObject Log;
+
+	Transform tempParent;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+    
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		tempParent = collision.gameObject.transform.parent;
+		collision.gameObject.transform.parent = Log.transform;
+	}
+
+	private void OnCollisionExit(Collision collision)
+	{
+		collision.gameObject.transform.parent = tempParent;
+	}
+}
