d0f2f2e1f06134c7dfddc795a5b9f817a567380b
Huskitch <Huskitch@users.noreply.github.com>
2019-12-02 14:39:59 +0000

    Some log changes

--- Assets/Scripts/LogFrogInteraction.cs
+++ Assets/Scripts/LogFrogInteraction.cs
@@ -5,30 +5,32 @@ using UnityEngine;
 public class LogFrogInteraction : MonoBehaviour
 {
 	public GameObject Log;
+	public GameObject Frog;
 
 	Transform tempParent;
 
-	// Start is called before the first frame update
+	public bool frogIsOnLog = false;
+
 	void Start()
 	{
 		
 	}
     
-
-    // Update is called once per frame
     void FixedUpdate()
     {
-        
-    }
+
+	}
 
 	private void OnCollisionEnter(Collision collision)
 	{
-		//if (collision.gameObject.tag == "Frog")
-		//{
-		//	print("Enter");
-		//	//tempParent = collision.gameObject.transform.parent;
-		//	collision.gameObject.transform.parent = this.gameObject.transform;// Log.transform;
-		//}
+		if (collision.gameObject.tag == "Frog")
+		{
+			Frog = collision.collider.gameObject;
+
+			Frog.transform.parent = transform;
+			Frog.GetComponent<Rigidbody>().isKinematic = true;
+			Frog.GetComponent<FrogMovement>().shouldMove = false;
+		}
 	}
 
 	//private void OnCollisionExit(Collision collision)
